adb4d298d065965c354272a386d1f2b3eb4e2208
Pierre Timmermans <ptim007@yahoo.com>
2019-10-17 22:41:05 +0200

    wip, grid19

--- roles/grid19/templates/01_gridSetup_prereq.sh
+++ roles/grid19/templates/01_gridSetup_prereq.sh
@@ -1,2 +1,2 @@
-{{ grid_oracle_home }}/gridSetup.sh -responseFile /home/{{ grid_osuser }}gridSetup.rsp -executePrereqs -silent -showProgress -waitforcompletion
+{{ grid_oracle_home }}/gridSetup.sh -responseFile /home/{{ grid_osuser }}/gridSetup.rsp -executePrereqs -silent -waitforcompletion
 
