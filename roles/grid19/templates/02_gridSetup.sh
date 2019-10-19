7aae488e71e4229fcf9b26688053be5b7eeaaf5d
Pierre Timmermans <ptim007@yahoo.com>
2019-10-19 14:40:51 +0200

    wip, adding oracle19 rac soft

---
+++ roles/grid19/templates/02_gridSetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+{{ grid_oracle_home }}/gridSetup.sh -responseFile {{ grid_osuser }}/gridSetup.rsp -silent -waitforcompletion
+
--- roles/orarac19/templates/01_gridSetup_prereq.sh
+++ roles/orarac19/templates/01_gridSetup_prereq.sh
@@ -1,4 +1,2 @@
-#!/bin/bash 
-
 {{ grid_oracle_home }}/gridSetup.sh -responseFile /home/{{ grid_osuser }}/gridSetup.rsp -executePrereqs -silent -waitforcompletion
 
